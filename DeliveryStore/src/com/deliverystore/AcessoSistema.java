7214921163a0fcaef63c4dbf8d09ed5f23909161
maykonaraujo <maykonaraujo23@gmail.com>
2014-02-18 21:01:46 -0300

    Inicio do projeto reformulado versão 2.0

---
+++ DeliveryStore/src/com/deliverystore/AcessoSistema.java
@@ -0,0 +1,22 @@
+package com.deliverystore;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class AcessoSistema extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.acessosistema);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.acesso_sistema, menu);
+		return true;
+	}
+
+}
